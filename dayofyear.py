5293085fe47d08128d507755997363851c22136d
Perry Peeters <perry@verspa.nl>
2017-04-28 14:28:45 +0200

    Add files via upload

---
+++ dayofyear.py
@@ -0,0 +1,3 @@
+from datetime import datetime
+day_of_year = datetime.now().timetuple().tm_yday
+print day_of_year
---
+++ modulo.py
@@ -0,0 +1,7 @@
+# modulo berekening in Python - PP 20150714
+
+from math import *
+x = int(raw_input("Te delen getal?" + "\n"))
+y = int(raw_input("Delen door?" + "\n"))
+a = x % y 
+print "Modulo waarde is: ", a
